20c8c81146ebdca7c9190979ce92d8db144253da
schrodingersket <schrodingersket@gmail.com>
2022-08-28 14:54:24 -0600

    MATH5670: Completed Homework 1.

--- 2022/fall/MATH5670_ScientificComputationI/Homework1/problem_2.m
+++ 2022/fall/MATH5670_ScientificComputationI/Homework1/problem_2.m
@@ -9,30 +9,37 @@ x_bar = 1;
 % Function definitions
 %
 u = @(x) sin(2*x);
-upp = @(x) -4*sin(2*x);
+uxx = @(x) -4*sin(2*x);
+ux6 = @(x) -(2^6)*sin(2*x);
 dd_4 = @(x, h) (1/h^2) * ((-1/12)*u(x-2*h) + (4/3)*u(x-h) + (-5/2)*u(x) + (4/3)*u(x+h) + (-1/12)*u(x+2*h));
 
-upp_true = upp(x_bar);
+uxx_true = uxx(x_bar);
 hvals = logspace(-1, -4, 13);
 E_DD4u = [];
+E_DD4u_predicted = [];
 
 % table headings:
 disp(' ')
-disp('       h              DD4u')
+disp('       h           Error [DD4u]    Predicted Error [DD4u]')
 
 for i=1:length(hvals)
    h = hvals(i);
 
-   % Approximation to u''(1):
+   % Fourth-order approximation to u''(x_bar):
    %
-   DD4u = dd_4(1, h);
+   DD4u = dd_4(x_bar, h);
 
    % Error:
    %
-   E_DD4u(i) = DD4u - upp_true;
+   E_DD4u(i) = DD4u - uxx_true;
 
-   % print line of table:
-   disp(sprintf('%13.4e   %13.4e', h, E_DD4u(i)))
+   % Predicted error:
+   %
+   E_DD4u_predicted(i) = (-1/90) * h^4 * ux6(x_bar);
+
+   % Print line of table:
+   %
+   disp(sprintf('%13.4e   %13.4e   %13.4e', h, E_DD4u(i), E_DD4u_predicted(i)))
 end
 
 % Plot errors
@@ -40,4 +47,8 @@ end
 clf;
 axis([5e-4 .2 1e-12 1]);
 plt = loglog(hvals, abs(E_DD4u), 'o-');
+grid on;
+title('Step size vs. Absolute Error')
+xlabel('Step size (h)')
+ylabel('Error (L1)')
 waitfor(plt);
