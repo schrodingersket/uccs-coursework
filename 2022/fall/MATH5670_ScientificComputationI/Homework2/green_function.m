1a2cc9a792ed6c2d455eccf070169c67ce87a92b
schrodingersket <schrodingersket@gmail.com>
2022-09-05 18:31:57 -0500

    MATH5670: Completed Homework 2 Problem 1(c). Added solution skeletons for all other problems.

--- 2022/fall/MATH5670_ScientificComputationI/Homework2/green_function.m
+++ 2022/fall/MATH5670_ScientificComputationI/Homework2/green_function.m
@@ -1,17 +1,14 @@
-function G = green_function(xbar, x, f_xbar)
+function G = green_function(xbar, x)
     arguments
         xbar   (1,1) double {mustBeNonnegative,mustBeLessThanOrEqual(x_bar, 1)}
         x      (1,:) double {mustBeNonnegative,mustBeLessThanOrEqual(x, 1)}
-        f_xbar (1,1) double
     end
 
-    gf = zeros(1, length(x));
+    G = zeros(1, length(x));
     for i=1:1:length(x)
         if x(i) <= xbar
-            gf(i) = (xbar - 1) .* x(i);
+            G(i) = (xbar - 1) .* x(i);
         else
-            gf(i) = xbar .* (x(i) - 1);
+            G(i) = xbar .* (x(i) - 1);
     end
-
-    G = f_xbar .* gf;
 end
--- 2022/fall/MATH5670_ScientificComputationI/Homework2/problem_1.m
+++ 2022/fall/MATH5670_ScientificComputationI/Homework2/problem_1.m
@@ -3,7 +3,7 @@ x = linspace(0, 1, 20 * grid_points);
 xbar = linspace(0, 1, grid_points);
 
 f = @(z) z;
-f_true = @(z) (1/6) .* (z.^3) - (1/6) .* z;
+f_true = @(z) ((1/6) .* (z.^3) - (1/6) .* z);
 
 % Plot Green's functions
 %
@@ -11,13 +11,13 @@ clf;
 y = zeros(grid_points, length(x));
 
 for i = 1:1:grid_points
-    y(i,:) = green_function(xbar(i), x, f(xbar(i)));
-    plt = plot(x, y(i,:), '--', 'DisplayName', sprintf('G(x, %0.5f)', xbar(i)));
+    y(i,:) = f(xbar(i)) .* green_function(xbar(i), x);
+    plt = plot(x, y(i,:), '--', 'DisplayName', sprintf('G(x, %0.2f)', xbar(i)));
     hold on;
 end
 
-plt = plot(x, sum(y), 'DisplayName', 'F(x)');
-plt = plot(x, f_true(x), 'DisplayName', 'u(x)');
+plt = plot(x, sum(y), 'DisplayName', 'U(x)');
+% plt = plot(x, f_true(x), 'DisplayName', 'u(x)');
 
 
 title("Green's Functions");
