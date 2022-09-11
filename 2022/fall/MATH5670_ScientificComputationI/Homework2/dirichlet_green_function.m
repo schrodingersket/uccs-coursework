1e46bf800808ba77f36046ac8d57dad299a159d8
schrodingersket <schrodingersket@gmail.com>
2022-09-11 15:12:39 -0600

    MATH5670: Completed Homework 2 Problem 2(a).

--- 2022/fall/MATH5670_ScientificComputationI/Homework2/dirichlet_green_function.m
+++ 2022/fall/MATH5670_ScientificComputationI/Homework2/dirichlet_green_function.m
@@ -1,4 +1,4 @@
-function G = green_function(xbar, x)
+function G = dirichlet_green_function(xbar, x)
     arguments
         xbar   (1,1) double {mustBeNonnegative,mustBeLessThanOrEqual(x_bar, 1)}
         x      (1,:) double {mustBeNonnegative,mustBeLessThanOrEqual(x, 1)}
@@ -7,7 +7,7 @@ function G = green_function(xbar, x)
     G = zeros(1, length(x));
     for i=1:1:length(x)
         if x(i) <= xbar
-            G(i) = (xbar - 1) .* x(i);
+            G(i) = x(i) .* (xbar - 1);
         else
             G(i) = xbar .* (x(i) - 1);
     end
--- 2022/fall/MATH5670_ScientificComputationI/Homework2/problem_1.m
+++ 2022/fall/MATH5670_ScientificComputationI/Homework2/problem_1.m
@@ -5,6 +5,8 @@ grid_points = 5;
 h = (b - a) / (grid_points - 1);
 x = linspace(0, 1, 20 * grid_points);
 xbar = linspace(0, 1, grid_points);
+alpha = 0;
+beta = 0;
 
 f = @(z) z;
 f_true = @(z) ((1/6) .* (z.^3) - (1/6) .* z);
@@ -15,13 +17,20 @@ clf;
 y = zeros(grid_points, length(x));
 
 for i = 1:1:grid_points
-    y(i,:) = h * f(xbar(i)) .* green_function(xbar(i), x);
-    plt = plot(x, y(i,:), '--', 'DisplayName', sprintf('G(x, %0.2f)', xbar(i)));
+    y(i,:) = h * f(xbar(i)) .* dirichlet_green_function(xbar(i), x);
+    plt = plot(x, y(i,:), '--', 'DisplayName', sprintf('h * f(%0.2f) * G(x, %0.2f)', xbar(i), xbar(i)));
     hold on;
 end
 
-plt = plot(x, sum(y), 'DisplayName', 'U(x)');
-plt = plot(x, f_true(x), 'DisplayName', 'u(x)');
+% Plot discrete approximation U
+%
+plt = plot(x, alpha*(1 - x) + beta*x + sum(y), 'DisplayName', 'U(x)');
+
+% Plot exact solution for alpha = beta = 0
+%
+if alpha == 0 && beta == 0
+    plt = plot(x, f_true(x), 'DisplayName', 'u(x)');
+end
 
 
 title("Green's Functions");
