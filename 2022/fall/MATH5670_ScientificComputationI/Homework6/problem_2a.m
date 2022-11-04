7ff2e36cee912b78590dfe921be5a98dfafb871c
schrodingersket <schrodingersket@gmail.com>
2022-11-04 16:14:34 -0600

    MATH5670: Completed Homework 6 Problem 2(a).

---
+++ 2022/fall/MATH5670_ScientificComputationI/Homework6/problem_2a.m
@@ -0,0 +1,15 @@
+T = 1;
+
+step_sizes = [19 29 39 49, 99];
+h_vec = zeros(length(step_sizes), 1);
+k_vec = zeros(length(step_sizes), 1);
+err_vec = zeros(length(step_sizes), 1);
+
+for i=1:length(step_sizes)
+    [h_vec(i), k_vec(i), err_vec(i)] = heat_CN(step_sizes(i));
+end
+
+disp('        h            k          error')
+disp([h_vec, k_vec, err_vec]);
+
+error_loglog(h_vec, err_vec, 'problem_2a_heatCN_error');
