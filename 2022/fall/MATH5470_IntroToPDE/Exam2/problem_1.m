84093d61b046742c72e82407c6c5c0bc1f352d29
schrodingersket <schrodingersket@gmail.com>
2022-11-08 17:50:59 -0700

    MATH5470: Completed Exam 2 take-home problem.

---
+++ 2022/fall/MATH5470_IntroToPDE/Exam2/problem_1.m
@@ -0,0 +1,24 @@
+[X,Y] = meshgrid(linspace(0, 4, 50), linspace(0, 2, 50));
+
+approximation_terms = 100;
+u = @(x, y, n) (4/(n * pi * sinh(-2 * n * pi))) .* sinh(n * pi * (x - 4) / 2) .* sin(n * pi * y / 2);
+
+figure;
+
+Z = u(X, Y, 1);
+
+if approximation_terms > 1
+    for n=2:approximation_terms
+        if mod(n, 2) ~= 0
+            Z = Z + u(X, Y, n);
+        end
+    end
+end
+
+surf(X,Y,Z);
+title('$\Delta u = 0$', 'interpreter', 'latex', 'VerticalAlignment', 'bottom');
+colorbar();
+view(2);
+
+print('-dpng', 'problem_1');
+
