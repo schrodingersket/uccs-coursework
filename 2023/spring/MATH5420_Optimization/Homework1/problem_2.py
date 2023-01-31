4eb153672bae1ab846b4015d7dac33e59fa480f2
schrodingersket <schrodingersket@gmail.com>
2023-01-31 01:25:01 -0700

    MATH5420: Completed Homework 1.

--- 2023/spring/MATH5420_Optimization/Homework1/problem_2.py
+++ 2023/spring/MATH5420_Optimization/Homework1/problem_2.py
@@ -2,15 +2,13 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-x1 = np.linspace(0, 2, 100)
+x1 = np.linspace(-2, 2, 100)
 
 fig, ax = plt.subplots()
 
 def g1(x):
     return np.sqrt(4 - np.power(x, 2))
 
-def f(x):
-    return x
 
 # g1(x)
 #
@@ -20,12 +18,9 @@ plt.plot(x1, -1 * g1(x1), color='silver')
 # g2(x)
 #
 plt.axvline(1, color='gray', label=r'$x_1^2 - 1 \geq 0$')
+plt.axvline(-1, color='gray')
 
-# f(x)
-#
-plt.plot(x1, f(x1), color='black', label=r'$f(x) = x_1$')
-
-plt.xlim((-0.1, np.pi))
+plt.xlim((-np.pi, np.pi))
 plt.ylim((-np.pi, np.pi))
 plt.xlabel(r'$x_1$')
 plt.ylabel(r'$x_2$')
@@ -41,7 +36,15 @@ plt.fill_between(
     alpha=0.25,
     label=r'$S$',
 )
+plt.fill_between(
+    x1,
+    g1(x1),
+    -g1(x1),
+    where=x1 <= -1,
+    color='green',
+    alpha=0.25,
+)
 
-plt.legend()
+plt.legend(loc='upper right')
 plt.title(r'Feasible Region $S$ for $f(x)$')
 plt.savefig('{}.jpg'.format(os.path.splitext(os.path.basename(__file__))[0]))
