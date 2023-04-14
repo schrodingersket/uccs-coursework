2063f2ffc83d4f049bf810671183da1e10e6cd43
schrodingersket <schrodingersket@gmail.com>
2023-04-13 22:35:19 -0600

    MATH6680/Exam1: Fixed integrating factor in `problem_1.m`.

--- 2023/spring/MATH6680_ScientificComputationII/Exam1/problem_1.m
+++ 2023/spring/MATH6680_ScientificComputationII/Exam1/problem_1.m
@@ -1,8 +1,13 @@
-clear all;
+% clear all;
+
+global epsilon k;
+
+epsilon = 0.03;
 
 N = 64;
-dt = .1/N^2;
+dt = .4/N^2;
 x = (2*pi/N)*(-N/2:N/2-1)';
+k = [0:N/2-1 0 -N/2+1:-1]';
 
 % Initial condition
 %
@@ -24,27 +29,28 @@ nmax = round(tmax/dt);
 u_plot = u;
 t_plot = 0;
 
-% RHS of the ODE
-%
-function y = fourier_burgers(t, V)
-    epsilon = 0.03;
-
-    n = size(V, 1);
-    k = [0:n/2-1 0 -n/2+1:-1]';
-
-    y = (1i * k / 2) .* exp(epsilon .* k.^2 .* t) .* fft(real(ifft( exp(-epsilon * k.^2 * t) .* V)).^2);
-end
 
 % RK4 integration
 %
 yn = V0;
 tn = 0;
 
+% Set to 1 to use integrating factor
+%
+integrating_factor = 1;
+
 for n = 1:nmax
-    k1 = fourier_burgers(tn, yn);
-    k2 = fourier_burgers(tn + dt/2, yn + dt * k1/2);
-    k3 = fourier_burgers(tn + dt/2, yn + dt * k2/2);
-    k4 = fourier_burgers(tn + dt, yn + dt * k3);
+    if integrating_factor == 1
+        k1 = fourier_burgers_if(tn, yn);
+        k2 = fourier_burgers_if(tn + dt/2, yn + dt * k1/2);
+        k3 = fourier_burgers_if(tn + dt/2, yn + dt * k2/2);
+        k4 = fourier_burgers_if(tn + dt, yn + dt * k3);
+    else 
+        k1 = fourier_burgers(tn, yn);
+        k2 = fourier_burgers(tn + dt/2, yn + dt * k1/2);
+        k3 = fourier_burgers(tn + dt/2, yn + dt * k2/2);
+        k4 = fourier_burgers(tn + dt, yn + dt * k3);
+    end
 
     tn = n*dt;
     yn = yn + 1/6 * (k1 + 2*k2 + 2*k3 + k4) * dt;
@@ -52,13 +58,18 @@ for n = 1:nmax
     % Keep track of various time steps for plotting
     %
     if mod(n, nplt) == 0
-        u_plot = [u_plot real(ifft(yn))];
+        if integrating_factor == 1
+            u_plot = [u_plot real(ifft(exp(-epsilon * k.^2 * (n-1)*dt) .* yn))];
+        else
+            u_plot = [u_plot real(ifft(yn))];
+        end
         t_plot = [t_plot tn];
     end
 end
 
 % Plot results
 %
+figure;
 waterfall(x, t_plot, u_plot')
 colormap([0 0 0])
 view(-20, 25)
@@ -70,3 +81,22 @@ set(gca, 'ztick', [0, 1])
 pbaspect([1 1 .13])
 
 print('-dpng', 'problem_1')
+
+
+%% RK4 RHS functions
+
+function y = fourier_burgers(t, V)
+    global epsilon k;
+
+    % Burgers equation in Fourier space without integrating factor
+    %
+    y = (1i * k / 2) .* fft(real(ifft(V)).^2) - epsilon * k.^2 .* V;
+end
+
+function y = fourier_burgers_if(t, V)
+    global epsilon k;
+
+    % Burgers equation in Fourier space with integrating factor
+    %
+    y = (1i * k / 2) .* exp(epsilon .* k.^2 .* t) .* fft(real(ifft( exp(-epsilon * k.^2 * t) .* V)).^2);
+end
