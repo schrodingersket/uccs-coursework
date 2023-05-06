f7167a307ee851628005167411ad19664fc0dace
schrodingersket <schrodingersket@gmail.com>
2023-05-06 00:35:58 -0600

    MATH6680/Homework4: Completed assigned problem.

--- 2023/spring/MATH6680_ScientificComputationII/Homework4/bestfitnormal.m
+++ 2023/spring/MATH6680_ScientificComputationII/Homework4/bestfitnormal.m
@@ -1,33 +1,6 @@
 function out=bestfitnormal(data)
+    [M, dim]=size(data);
 
-[N, dim]=size(data);
-
-Const = ones(N,1); % Vector of the constant term in the RHS
-out = data\Const; % Find the coefficients
-
-switch dim
-    case 2
-        x=data(:,1); y=data(:,2); L=plot(x,y,'ro'); % Plot the original data points
-        % set(L,'Markersize',2*get(L,'Markersize')) % Making the circle markers larger
-        % set(L,'Markerfacecolor','r') % Filling in the markers
-        % hold on
-        % xx=min(x):0.1:max(x);
-        % yy=(1-out(1)*xx)/out(2);
-        % line(xx,yy) % plotting the line
-        % title(sprintf('Plotting line (%f)*x+(%f)*y=1',out(1), out(2)))
-        % hold off
-    case 3
-        x=data(:,1); y=data(:,2); z=data(:,3);
-        L=plot3(x,y,z,'ro'); % Plot the original data points
-        % set(L,'Markersize',2*get(L,'Markersize')) % Making the circle markers larger
-        % set(L,'Markerfacecolor','r') % Filling in the markers
-        % hold on
-        % [xx, yy]=meshgrid(min(x):0.1:max(x),min(y):0.1:max(y)); % Generating a regular grid for plotting
-        % zz = (1 - out(1) * xx - out(2) * yy)/out(3);
-        % surf(xx,yy,zz) % Plotting the surface
-        % title(sprintf('Plotting plane (%f)*x+(%f)*y+(%f)*z=1',out(1), out(2), out(3)))
-        % hold off
-    otherwise
-        fprintf('The data is not 2D or 3D')
-
+    Const = ones(M,1); % Vector of the constant term in the RHS
+    out = data\Const; % Find the coefficients
 end
--- 2023/spring/MATH6680_ScientificComputationII/Homework4/problem_1.m
+++ 2023/spring/MATH6680_ScientificComputationII/Homework4/problem_1.m
@@ -1,40 +1,99 @@
-clf
-clear all
+figure(1), clf
+t = (0:0.1:2*pi)';
+noiselevel = 0.1;
 
-rng(0)
+% Parametric Fernandez-Guasti squircle
+%
+r = 1;
+s = 0.95;
+x = r/(2*s) * sqrt(2 + 2*s*sqrt(2)*cos(t) + s^2*cos(2*t)) - r/(2*s)*sqrt(2 - 2*s*sqrt(2)*cos(t) + s^2*cos(2*t)) + noiselevel * rand(size(t));
+y = r/(2*s) * sqrt(2 + 2*s*sqrt(2)*sin(t) - s^2*cos(2*t)) - r/(2*s)*sqrt(2 - 2*s*sqrt(2)*sin(t) - s^2*cos(2*t));
 
-t = 0:0.1:2*pi;
+xzero = [x y];
 
-N = length(t); % Number of points in point cloud
-M = 2; % Number of points sampled to compute normal plane
+N = length(t); %total number of points in the point cloud
 
-x = 2*cos(t) + 0.1*rand(size(t));
-x = [x(1) x x(end)];
-y = 3*sin(t);
-y = [y(1) y y(end)];
+plot(x, y, 'k.')
+% axis equal
 
-xzero = [x(2:end-1)' y(2:end-1)'];
-
-hold on;
-% plot(x, y, 'k.')
+xzero = [xzero(end, :); xzero; xzero(1, :)];
 
 normal = zeros(N, 2);
 
-for j = 2:N-1
-    data = [ x(j-1:j+1)' y(j-1:j+1)' ];
-    normal(j, :) = bestfitnormal(data);
+for j=2:N+1
+    data = xzero(j-1:j+1,:);
+    normal(j-1,:) = bestfitnormal(data);
 end
 
-axis equal
+xzero([1, N + 2], :)=[];
+alpha=0.4;
+%exterior points
 
-alpha = 0.1;
+xplus=xzero + alpha*normal;
+xminus=xzero - alpha*normal;
+plot(xzero(:, 1), xzero(:,2), 'ro', 'DisplayName', 'x_j')
+hold on
+plot(xplus(:,1), xplus(:, 2), 'b*', 'DisplayName', 'x_j^+')
+plot(xminus(:,1), xminus(:, 2), 'gd', 'DisplayName', 'x_j^-')
+% legend()
 
-% Exterior/interior points
-%
-xplus = xzero + alpha*normal;
-xminus = xzero - alpha*normal;
+axis([-2 2 -2 2])
+print('problem_1i.png', '-dpng')
 
-plot(xplus(:, 1), xplus(:, 2), 'b*')
-plot(xminus(:, 1), xminus(:, 2), 'gd')
+figure(2), clf
+plot3(xzero(:, 1), xzero(:, 2), zeros(N, 1), 'ro', 'DisplayName', 'x_j')
+hold on
+plot3(xplus(:, 1), xplus(:, 2), alpha*ones(N, 1), 'b*', 'DisplayName', 'x_j^+')
+plot3(xminus(:, 1), xminus(:, 2), -alpha*ones(N, 1), 'gd', 'DisplayName', 'x_j^-')
+axis([-2 2 -2 2 -3*alpha 3*alpha])
+% legend()
+grid on
+hold on
 
-print('problem_1_point_cloud', '-dpng')
+eps = 10;
+% phi = @(r) exp(-(eps*r).^2); %GA
+% phi = @(r) 1./sqrt(1 + (eps*r).^2); %IMQ
+phi = @(r) sqrt(1 + (eps*r).^2); %MQ
+
+M = 3*N;
+RHS = [zeros(N,1); alpha*ones(N,1); -alpha*ones(N,1)];
+
+A = zeros(M);
+
+Xbig = [xzero; xplus; xminus];
+
+for j = 1:M
+    for k = 1:M
+        A(j,k) = phi(norm(Xbig(j,:)-Xbig(k,:)));
+    end
+end
+
+C = A\RHS;
+
+[Xplot, Yplot] = meshgrid(-4:0.05:4, -4:0.05:4);
+
+Zplot = zeros(size(Xplot));
+Dist = zeros(M, 1);
+
+rbf = @(Xin) C' * phi(sqrt(sum((Xbig - repmat(Xin, size(Xbig, 1), 1)).^2, 2)));
+
+for j = 1:size(Xplot, 1)
+    for k = 1:size(Yplot, 1)
+        Zplot(j,k)=rbf([Xplot(j,k) Yplot(j,k)]);
+    end
+end
+
+surf(Xplot, Yplot, Zplot, 'DisplayName', 'RBF Interpolant')
+print('problem_1ii.png', '-dpng')
+
+figure(3), clf
+plot(xzero(:, 1), xzero(:,2), 'ro', 'DisplayName', 'x_j')
+hold on
+plot(xplus(:,1), xplus(:,2), 'b*', 'DisplayName', 'x_j^+')
+plot(xminus(:,1), xminus(:,2), 'gd', 'DisplayName', 'x_j^-')
+axis([-2 2 -2 2])
+v = [-alpha, 0, alpha];
+v = [0, 0.001];
+contour(Xplot, Yplot, Zplot, v, 'DisplayName', 'RBF Interpolant')
+% legend()
+print('problem_1iii.png', '-dpng')
